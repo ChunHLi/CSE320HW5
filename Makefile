ab18c0c53b8f803023a31c429ad4a41017a75f4d
ChunHLi <chunhli@cs.stonybrook.edu>
2018-05-01 10:25:45 -0700

    added make clean, reverted code and added print statements

--- Makefile
+++ Makefile
@@ -4,4 +4,8 @@ main: main.c
 	gcc -g -pthread -o hw5_main main.c
 
 mem: mem.c
-	gcc -g -pthread -o mem mem.c 
+	gcc -g -pthread -o mem mem.c
+clean:
+	rm -f hw5_main mem
+	find . -type p -delete	
+
--- mem.c
+++ mem.c
@@ -30,14 +30,14 @@ int main(int argc, char** argv){
 	while(status){
 		char buf[255];
 		emu_server = open(src_emu_server,O_RDWR);
-		//read(emu_server,buf,sizeof(buf));
-		FILE * fp = fdopen(emu_server,"r+");
-		fgets(buf,255,fp);
-		printf("%s\n",buf);
+		read(emu_server,buf,sizeof(buf));
+		
+		printf("%s has a length of %lu\n",buf,strlen(buf));
                 int i = 0;
                 char** args;
 		char* token;
                 for (token=strtok(buf,","); token != NULL; token=strtok(NULL, ",")){
+			printf("token: %s\n",token);
                 	args[i++] = strdup(token);
                 }
 		printf("%s\n",args[0]);
